4d5751134b4f8e4b8d2d93e60a544f703fdb85a2
Vijay <reddyvijay1667@gmail.com>
2023-07-19 09:37:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,9 @@
-// Create a web server
-// npm install express --save
-// npm install body-parser --save
-// npm install mongoose --save
-// npm install cors --save
-// npm install morgan --save
-// npm install --save-dev nodemon
-// npm install bcryptjs --save
-// npm install jsonwebtoken --save
-// npm install multer --save
-// npm install --save-dev dotenv
-// npm install --save-dev eslint
-// npm install --save-dev eslint-config-airbnb-base eslint-plugin-import eslint-plugin-node
-// npm install --save-dev eslint-plugin-import eslint-plugin-node
-// npm install --save-dev eslint-config-prettier eslint-plugin-prettier
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb-base eslint-plugin-import eslint-plugin-node
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-prettier eslint-plugin-prettier
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-// npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-re
+
+const express = require('express');
+const app = express();
+
+
+app.get('/comments', (req, res) => {
+    res.json({"comments": "This is a comment"});
+    }  
+)
