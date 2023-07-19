8b3e3b7ce949e01caab559598b35e765508a9817
Vijay <reddyvijay1667@gmail.com>
2023-07-19 09:31:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,54 @@
 // Create a web server
-// To do this, we will use the Express web server framework for Node.js
-// http://expressjs.com/
-// To install Express:
-var express = require('express');
-// npm install --save express
-// Create a server.js file in the root of your application directory
-// In server.js, add the following code:
+// 1. Create a web server
+// 2. Create a route for GET /comments
+// 3. Create a route for GET /comments/:id
+// 4. Create a route for POST /comments
+// 5. Create a route for PUT /comments/:id
+// 6. Create a route for DELETE /comments/:id
+// 7. Create a route for GET /comments/:id/author
+// 8. Create a route for GET /comments/:id/post
 
-var app = express();
-var port = process.env.PORT || 3000;
-app.use(express.static(__dirname + '/app'));
-app.listen(port);
-console.log('Listening on port: ' + port);
-// Start the server
-// node server.js
-// Navigate to http://localhost:3000/
-// You should see the Angular app running in your browser
-// Set up the back-end
-// We will use the Firebase JavaScript library to interact with Firebase
-// https://www.firebase.com/docs/web/libraries/angular/quickstart.html
-// To install Firebase:
-// npm install --save firebase
-// In server.js, add the following code:
-// var Firebase = require('firebase');
-// var myFirebaseRef = new Firebase('https://flickering-fire-5944.firebaseio.com/');
-// myFirebaseRef.set({
-//   title: 'Hello World!',
+const express = require('express');
+const db = require('../db');
+
+const router = express.Router();
+
+// 2. Create a route for GET /comments
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM comments')
+    .then((result) => {
+      res.json(result.rows);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// 3. Create a route for GET /comments/:id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await db.query('SELECT * FROM comments WHERE id = $1', [id]);
+    if (result.rows.length) {
+      res.json(result.rows[0]);
+    } else {
+      res.status(404).json({ error: `Cannot find comment with id ${id}` });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// 4. Create a route for POST /comments
+router.post('/', async (req, res) => {
+  const { content, post_id, author_id } = req.body;
+  try {
+    const result = await db.query(
+      'INSERT INTO comments (content, post_id, author_id) VALUES ($1, $2, $3) RETURNING *',
+      [content, post_id, author_id]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
